5a959e067ec0a22475e967b713a8307d11658aee
Trey <trey.shearin3@gmail.com>
2024-01-14 20:25:56 -0500

    added 4 arrays to hold different character types

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var spec = ["!", "@", "#", "$", "%", "^", "&", "*", "-"]
 
 // Get references to the #generate element, #generate is button id
 var generateBtn = document.querySelector("#generate");
@@ -11,7 +14,9 @@ function writePassword() {
   var lowerCase = prompt("Would you like to include lowercase characters (Yes or No)?");
   var numeric = prompt("Would you like to include numeric characters (Yes or No)?");
   var specialCharacters = prompt("Would you like to include special characters (Yes or No)?");
-  var password = generatePassword();
+  console.log(writePassword.length);
+
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
 
