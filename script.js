8270b1d38426e06f03a5d591fb472b73f2196594
Trey <trey.shearin3@gmail.com>
2024-01-14 19:53:33 -0500

    Added prompts for every password characteristic to writePassword function declaration

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("How may characters does your password need to be (between 8 & 128)?");
+  var upperCase = prompt("Would you like to include uppercase characters (Yes or No)?");
+  var lowerCase = prompt("Would you like to include lowercase characters (Yes or No)?");
+  var numeric = prompt("Would you like to include numeric characters (Yes or No)?");
+  var specialCharacters = prompt("Would you like to include special characters (Yes or No)?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
