806afca75cecca6477d93dac6aad062aa98415df
Trey <trey.shearin3@gmail.com>
2024-01-14 19:29:37 -0500

    added additional comments to js for clarity

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+// Get references to the #generate element, #generate is button id
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
